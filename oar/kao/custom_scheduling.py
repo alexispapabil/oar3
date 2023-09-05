3f8b698b0097e5485f2f734bf033ff04bd4907a5
alexispapabil <alexispapabil@gmail.com>
2023-09-05 09:35:09 +0300

    Added custom policies

--- oar/kao/custom_scheduling.py
+++ oar/kao/custom_scheduling.py
@@ -1,6 +1,10 @@
 from procset import ProcSet
 
-from oar.lib.hierarchy import find_resource_hierarchies_scattered
+import oar.kao.scheduling
+from oar.lib import get_logger
+from oar.lib.hierarchy import find_resource_hierarchies_scattered,extract_n_scattered_block_itv
+
+logger = get_logger("oar.kamelot")
 
 '''
 Find the path leading to a resource within the hierarchy.
@@ -52,5 +56,112 @@ def compact(itvs_slots, hy_res_rqts, hy, beginning_slotset):
             result = result | res
         else:
             return ProcSet()
+
+    return result
+
+
+#def update(init, cur, i, bottom):
+    #j = i + 1
+
+    #if cur[i+1] == 0:
+        #cur[i+1] = init[i+1]
+        #cur[i] = 0 if cur[i] < 0 else cur[i]-1
+
+    #elif cur[j] < 0:
+        #if cur[i] > 0:
+            #while j <= bottom and cur[j] <= 0:
+                #j += 1
+            #if j > bottom:
+                #cur[i] -= 1
+            #else:
+                #return 1
+
+    #return 0
+
+
+def pick_n(avail, hier, rq, i, policy):
+    j = 0
+    acc = ProcSet()
+    l = len(hier)
+
+    cap = l//2 if policy == 'spread' else l
+
+    while j < l and rq[i] != 0 and cap > 0:
+        r = hier[j]
+        if r & avail == r:
+            cap -= 1
+            acc = acc | r
+            if rq[i] > 0:
+                rq[i] -= 1
+        j += 1
+    
+    if rq[i] < 0 and cap > 0:
+        return ProcSet()
+    
+    return acc
+
+def traversal(avail, hier_levels, hier, init, cur, lv, bottom, policy):
+    i = 0
+    acc = []
+    l = len(hier)
+
+    while i < l and cur[lv] != 0:
+        r = hier[i]
+        logger.info(r)
+        logger.info(init)
+        logger.info(cur)
+        children = [c for c in hier_levels[lv+1] if c.issubset(r)]
+        if lv+1 == bottom:
+            res = pick_n(avail, children, cur, lv+1, policy)
+        else:
+            res = traversal(avail, hier_levels, children, init, cur, lv+1, bottom, policy)
+        if res:
+            acc.insert(0, res)
+            if cur[lv+1] == 0:
+                cur[lv+1] = init[lv+1]
+                cur[lv] = 0 if cur[lv] < 0 else cur[lv]-1
+            elif cur[lv+1] < 0:
+                if lv+1 == bottom:
+                    if cur[lv] > 0:
+                        cur[lv] -= 1
+                else:
+                    if (policy == 'compact' and len(acc) == len(children)) or len(acc) == 2*len(children):
+                        cur[lv] = 0 if cur[lv] < 0 else cur[lv]-1
+        i += 1
+    
+    if lv == 0 and all(v != 0 for v in cur):
+        return []
+    return acc
+
+def my_find(itvs_slots, hy_res_rqts, hy, beginning_slotset, *args):
+
+    result = ProcSet()
+    
+    hy_keys, hy_levels = [list(t) for t in zip(*hy.items())]
+    hy_nbs = [-1 for lv in hy_levels]
+
+    for hy_res_rqt in hy_res_rqts:
+        (hy_level_nbs, constraints) = hy_res_rqt
+        for hy_l_n in hy_level_nbs:
+            (l_name, n) = hy_l_n
+            hy_nbs[hy_keys.index(l_name)] = n
+
+    itvs_cts_slots = constraints & itvs_slots
+    hy_levels[0].sort(key=lambda n: len(n & itvs_cts_slots), reverse = True)
+    
+    rset = traversal(itvs_cts_slots, hy_levels, hy_levels[0], hy_nbs, hy_nbs[:], 0, len(hy_levels)-2, args[0])
+
+    rset = [i for sublist in rset for i in sublist]
+    
+    res = ProcSet()
+    
+    for s in rset:
+        res = res | s 
+
+    logger.info(res)
+    if res:
+        result = result | res
+    else:
+        return ProcSet()
 
     return result
